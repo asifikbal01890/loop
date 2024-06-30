e5312584e13ac7a3697406cf469ba3f230a8771e
asifikbal01890 <asifikbal01890@gmail.com>
2024-06-30 16:55:18 +0600

    first commit

---
+++ forLoop.js
@@ -0,0 +1,29 @@
+/* var number = 0;
+
+while (number <= 10 ) {
+    console.log(number)
+   number++
+}  */
+
+/* for (var number = 0; number <= 5; number++){
+    console.log(number)
+}
+ */
+
+/* for(var i = 0; i <= 5; i++){
+    console.log(i);
+} */
+
+
+
+    // even numbers
+  /*   for(var i = 0; i <= 10; i += 2){
+        console.log(i);
+    }
+     */
+    
+    // odd numbers
+    for(var i = 1; i <= 10; i += 2){
+        console.log(i);
+    }   
+   
---
+++ while.js
@@ -0,0 +1,32 @@
+/***********************
+var roast = 0;
+
+while (roast <= 8 ) {
+    console.log(roast)
+    console.log("roast given");
+    roast++;
+} 
+*******************************/
+
+
+// even number
+/*
+ var number = 0;
+
+while (number <= 20 ) {
+    console.log(number)
+    // var number = number + 2;
+    number += 2;
+}  */
+
+
+    // odd numbers
+
+    var number = 1;
+
+    while (number <= 20 ) {
+        console.log(number)
+        // var number = number + 2;
+        number += 2;
+        // number++;
+    } 
